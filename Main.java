5f7ddaf5740c4e6a9d2fda4c698c31b381883a02
Ruth <88747761+AwareRuuh@users.noreply.github.com>
2022-10-08 14:18:52 +0000

    Tabuada do 1 ao 10

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main {
+ public static void main(String[] args) {
+  Scanner sc1 = new Scanner(System.in);
+  int valor;
+  System.out.println("Digite a tabuada que deseja obter: ");
+   valor = sc1.nextInt();
+   for (int i = 1; i <= 10; i++) {
+     System.out.println(valor + " x " + i + " = " + (valor*i));
+   }
+  }
+}
